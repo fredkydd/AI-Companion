87a0e4401694aa139f51778319efcdd8b3415a28
illusion <17003685+fredkydd@users.noreply.github.com>
2023-10-13 15:28:18 +0400

    Created base using Next.js RouteGroup

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+  return (
+    <>
+      <main>
+        <p className="text-red-500 text-3xl text-center mt-40">
+          Login Page (Unprotected)
+        </p>
+      </main>
+    </>
+  );
+}
---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+export default function RootPage() {
+  return (
+    <>
+      <main className="text-green-500 text-3xl">
+        <p>
+          Root Page inside of the (root) using Router Group (protected)
+          app/(root)/page.tsx === app/page.tsx
+        </p>
+      </main>
+    </>
+  );
+}
