7622b20cbccec1727c9b2b2bd7166dcff91d22d7
illusion <17003685+fredkydd@users.noreply.github.com>
2023-10-14 13:59:03 +0400

    fixed middleware, sign in && sign up are now working

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,9 @@
+import { UserButton } from '@clerk/nextjs';
 export default function RootPage() {
   return (
     <>
-      <main className="text-green-500 text-3xl">
-        <p>
-          Root Page inside of the (root) using Router Group (protected)
-          app/(root)/page.tsx === app/page.tsx
-        </p>
+      <main className="text-green-500 text-3xl m-3">
+        <UserButton afterSignOutUrl="/" />
       </main>
     </>
   );
