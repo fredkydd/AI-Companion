108aa23d408928f5f782af374b1059aa1e41ce8c
illusion <17003685+fredkydd@users.noreply.github.com>
2023-10-17 03:24:20 +0400

    added Customizable Shadcnui Button to the Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,9 @@
+'use client';
 import { cn } from '@/lib/utils';
 import { UserButton } from '@clerk/nextjs';
-import { Menu } from 'lucide-react';
+import { Menu, Sparkles } from 'lucide-react';
 import { Poppins } from 'next/font/google';
+import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
 const font = Poppins({
@@ -14,17 +16,25 @@ export default function Navbar() {
     <nav className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
       <section className="flex items-center">
         <Menu className="block md:hidden" />
-        <Link href="/" />
-        <h1
-          className={
-            (cn('hidden md:block text-xl md:text-3xl font-bold text-green-500'),
-            font.className)
-          }
-        >
-          companion.ai
-        </h1>
+        <Link href="/">
+          <h1
+            className={
+              (cn(
+                'hidden md:block text-xl md:text-3xl font-bold text-green-500'
+              ),
+              font.className)
+            }
+          >
+            companion.ai
+          </h1>
+        </Link>
       </section>
       <section className="flex items-center gap-x-3">
+        <Button variant="illusion" size="sm">
+          {'  '}
+          Upgrade <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+          {'  '}
+        </Button>
         <UserButton afterSignOutUrl="/" />
       </section>
     </nav>
